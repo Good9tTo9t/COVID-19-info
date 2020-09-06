769090ca0527ddf685a3ffdbebfaf4bd4607fcb3
HiroOnGitHub <49119877+HiroOnGithub@users.noreply.github.com>
2020-09-06 20:16:34 +0800

    POLISH

--- covid19.py
+++ covid19.py
@@ -1,11 +1,13 @@
 import requests
 data = requests.get('https://covid19-api.org/api/status').json()
-country_iq = input("請輸入要查詢的國家代號(如:TW): ")
-for i in range(len(data)):
-    if data[i]['country'] == country_iq:
-        print("國家：%s" %(data[i]['country']))
-        print("更新時間：%s" %(data[i]['last_update']))
-        print("確診人數：%s" %(data[i]['cases']))
-        print("死亡人數：%s" %(data[i]['deaths']))
-        print("痊癒人數：%s" %(data[i]['recovered']))
-input("Press Enter To Exit...")
+country_iq = ''
+print("Made by Hiro Wu with ❤")
+while country_iq != '0':
+    country_iq = input("請輸入要查詢的國家代號(如:tw)，或按0來離開: ")
+    for i in range(len(data)):
+        if data[i]['country'] == country_iq.upper():
+            print("國家：%s" %(data[i]['country']))
+            print("更新時間：%s" %(data[i]['last_update'].replace("T"," ")))
+            print("確診人數：%s" %(data[i]['cases']))
+            print("死亡人數：%s" %(data[i]['deaths']))
+            print("痊癒人數：%s" %(data[i]['recovered']))
